ee7e4efd717bd89be2a2db3e1d9eb17d1cff2770
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-07-16 15:57:46 +0200

    exercises table of contents

---
+++ exercises/README.md
@@ -0,0 +1,25 @@
+
+
+<!-- BEGIN TOC -->
+- [/about-testing](./about-testing)
+  - [/1-expect.js](./about-testing/1-expect.js)
+  - [/2-it.js](./about-testing/2-it.js)
+  - [/3-describe.js](./about-testing/3-describe.js)
+  - [/4-deep-equality.js](./about-testing/4-deep-equality.js)
+  - [/5-more-assertions.js](./about-testing/5-more-assertions.js)
+- [/block-scope](./block-scope)
+  - [/asdf.js](./block-scope/asdf.js)
+  - [/const-let.js](./block-scope/const-let.js)
+  - [/block-scope-copy](./block-scope/block-scope-copy)
+    - [/asdf.js](./block-scope/block-scope-copy/asdf.js)
+    - [/const-let.js](./block-scope/block-scope-copy/const-let.js)
+- [/object-literals](./object-literals)
+  - [/bracket-notation.js](./object-literals/bracket-notation.js)
+  - [/computed-properties.js](./object-literals/computed-properties.js)
+  - [/destructuring.js](./object-literals/destructuring.js)
+  - [/dot-notation.js](./object-literals/dot-notation.js)
+  - [/es6-shorthand.js](./object-literals/es6-shorthand.js)
+- [/primitive-types](./primitive-types)
+  - [/type-and-value.js](./primitive-types/type-and-value.js)
+
+<!-- END TOC -->
--- scripts/register.js
+++ scripts/register.js
@@ -115,3 +115,41 @@ console.log('\n--- writing /index.json ---\n');
 // write the file
 const stringifiedReg = JSON.stringify(registered, null, '  ');
 fs.writeFileSync(path.normalize(path.join(__dirname, '..', 'index.json')), stringifiedReg);
+
+
+const tableOfContents = (virDir, path, indent) => {
+  indent = indent || '';
+  path = path || '';
+  const dirList = virDir.dirs
+    ? virDir.dirs
+      .map(subDir => {
+        const subIndex = tableOfContents(subDir, path + subDir.path, indent + '  ');
+        const reviewPath = path + subDir.path;
+        return `${indent}- [${subDir.path}](.${reviewPath})`
+          + (subIndex ? '\n' + subIndex : '\n');
+      })
+      .reduce((list, li) => list + li, '')
+    : '';
+
+  const filesList = virDir.files
+    ? virDir.files
+      .map(file => {
+        const reviewPath = path + file.path;
+        return `${indent}- [${file.path}](.${reviewPath})\n`
+      })
+      .reduce((list, li) => list + li, '')
+    : '';
+  return filesList + dirList;
+}
+
+const newToc = tableOfContents(registered);
+const tocRegex = /(<!--[ \t]*BEGIN TOC[ \t]*-->)([^;]*)(<!--[ \t]*END TOC[ \t]*-->)/;
+const tocReplacer = `<!-- BEGIN TOC -->\n${newToc}\n<!-- END TOC -->`;
+const readmePath = path.normalize(path.join(EXERCISES_DIR, 'README.md'));
+const oldReadme = fs.existsSync(readmePath)
+  ? fs.readFileSync(readmePath, 'utf-8')
+  : '';
+const newReadme = oldReadme.match(tocRegex)
+  ? oldReadme.replace(tocRegex, tocReplacer)
+  : `${oldReadme}\n\n${tocReplacer}`;
+fs.writeFileSync(readmePath, newReadme);
